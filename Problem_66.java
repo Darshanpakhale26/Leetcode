eecd58d46a6de7b0cec6fe199307cc160ed236d9
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-10-26 16:21:01 +0530

    first commit

---
+++ Problem_66.java
@@ -0,0 +1,16 @@
+// 66 Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+class Problem_66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
