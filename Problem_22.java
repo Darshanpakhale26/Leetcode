d518644442da486908764b77348f79a05b1ba248
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-10-30 23:14:44 +0530

    first commit

---
+++ Problem_22.java
@@ -0,0 +1,24 @@
+// Problem 22 Generate all possible valid parentheses combinations for a given number of pairs.
+
+public class Problem_22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+
+}
