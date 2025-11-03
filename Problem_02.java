9e39202b92a81f8160acd11e886b64433dd72c65
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-11-03 23:39:37 +0530

    first commit

---
+++ Problem_02.java
@@ -0,0 +1,60 @@
+// 2 add two numbers
+
+class Problem_02 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, current = dummyHead;
+        int carry = 0;
+        
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        
+        return dummyHead.next;
+    }
+}
+
+
+// other approach without using dummy head
+
+
+class Problem_02_No_Dummy {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        
+        return head;
+    }
+}
