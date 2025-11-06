7f24830cc4a6efb6a608e1f20533547a74be566c
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-11-06 10:21:24 +0530

    first commit

--- Problem_744.java
+++ Problem_744.java
@@ -17,3 +17,17 @@ class Solution {
         return letters[left % letters.length];
     }
 }
+
+
+// Another approach using linear search
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (char letter : letters) {
+            if (letter > target) {
+                return letter;
+            }
+        }
+        return letters[0];
+    }
+}
