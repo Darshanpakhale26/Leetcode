0f74798c9c8cf70c81e0ec0025696ca2fd8f7419
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-11-07 19:30:02 +0530

    first commit

---
+++ Problem_23.java
@@ -0,0 +1,51 @@
+// 23 Merge k Sorted Lists
+
+import java.util.PriorityQueue;
+public class Problem_23 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add the head of each list to the priority queue
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHeap.offer(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode smallestNode = minHeap.poll();
+            current.next = smallestNode;
+            current = current.next;
+
+            if (smallestNode.next != null) {
+                minHeap.offer(smallestNode.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
