230f6cba5ad7d86395087a89af8db41a58eb4d1e
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-12-11 23:48:43 +0530

    first commit

---
+++ Problem_88.java
@@ -0,0 +1,24 @@
+// Problem 88 Merge Sorted Array
+
+
+class Problem_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
+
+
