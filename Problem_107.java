a86c4cf094f074b8fab4fe3898ac5855f9e03ff6
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-12-18 22:58:05 +0530

    first commit

---
+++ Problem_107.java
@@ -0,0 +1,36 @@
+// Problem 107: Binary Tree Level Order Traversal
+
+
+class Problem_107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(0, currentLevel); // Add the current level at the beginning
+        }
+
+        return result;
+    }
+}
+
+
