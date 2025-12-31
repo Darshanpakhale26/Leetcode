e62fe5ca2d827346e00dc37212d29407cc6ce8e5
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2025-12-31 11:29:07 +0530

    first commit

---
+++ Problem_206.java
@@ -0,0 +1,22 @@
+// 206 Reverse Linked List
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Problem_206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        
+        while (current != null) {
+            ListNode nextTemp = current.next; // Store next node
+            current.next = prev;               // Reverse the link
+            prev = current;                    // Move prev to current
+            current = nextTemp;                // Move to next node
+        }
+        
+        return prev; // New head of the reversed list
+    }
+}
