ec3fa2731fc616cc5e2953eb555c8dc1478138d9
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-01-06 23:43:53 +0530

    first commit

---
+++ Problem_700.java
@@ -0,0 +1,15 @@
+// Problem 700 : Search in a Binary Search Tree
+class Problem_700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
