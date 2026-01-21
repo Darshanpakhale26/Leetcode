aab2f665c1cb804b555e0528bef3449e789999ae
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-01-21 22:16:05 +0530

    first commit

---
+++ Problem_136.java
@@ -0,0 +1,13 @@
+// Problem 136: Single Number
+
+
+class Problem_136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
+
