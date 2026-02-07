98419e66cb0561a1720d2626d7a984fe7fa7ce8b
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-02-07 19:37:30 +0530

    first commit

--- Problem_450.java
+++ Problem_450.java
@@ -2,26 +2,17 @@
 
 class Problem_450 {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
-        }
-
+        if (root == null) return null;
         if (key < root.val) {
             root.left = deleteNode(root.left, key);
         } else if (key > root.val) {
             root.right = deleteNode(root.right, key);
         } else {
-            // Node to be deleted found
-            if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
-            } else {
-                // Node with two children: Get the inorder successor (smallest in the right subtree)
-                TreeNode successor = findMin(root.right);
-                root.val = successor.val; // Copy the inorder successor's value to this node
-                root.right = deleteNode(root.right, successor.val); // Delete the inorder successor
-            }
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
         }
         return root;
     }
@@ -32,4 +23,6 @@ class Problem_450 {
         }
         return node;
     }
+    
 }
+
