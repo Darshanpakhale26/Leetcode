ab4ff02f7d6113ea31a5aad58364c4cc815d8f21
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-03-02 00:33:13 +0530

    updated

--- Problem_03.java
+++ Problem_03.java
@@ -1,30 +1,30 @@
 // 3 Longest Substring Without Repeating Characters
 
-import java.util.HashMap;
-class Problem_03 {
-    public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> charIndexMap = new HashMap<>();
-        int left = 0;
-        int maxLength = 0;
+// import java.util.HashMap;
+// class Problem_03 {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashMap<Character, Integer> charIndexMap = new HashMap<>();
+//         int left = 0;
+//         int maxLength = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            char currentChar = s.charAt(right);
-            if (charIndexMap.containsKey(currentChar)) {
-                // Move the left pointer to the right of the last occurrence of currentChar
-                left = Math.max(left, charIndexMap.get(currentChar) + 1);
-            }
-            charIndexMap.put(currentChar, right);
-            maxLength = Math.max(maxLength, right - left + 1);
-        }
+//         for (int right = 0; right < s.length(); right++) {
+//             char currentChar = s.charAt(right);
+//             if (charIndexMap.containsKey(currentChar)) {
+//                 // Move the left pointer to the right of the last occurrence of currentChar
+//                 left = Math.max(left, charIndexMap.get(currentChar) + 1);
+//             }
+//             charIndexMap.put(currentChar, right);
+//             maxLength = Math.max(maxLength, right - left + 1);
+//         }
 
-        return maxLength;
-    }
-}
+//         return maxLength;
+//     }
+// }
 
 // without using collections
  
 import java.util.Arrays;
-import java.util.List;
+// import java.util.List;
 
 class Problem_03_No_Collections {
     public int lengthOfLongestSubstring(String s) {
