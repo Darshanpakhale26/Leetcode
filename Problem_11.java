02325418ca8d2605001705ce92b1179e1a37ee7a
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-03-02 00:19:34 +0530

    first commit

---
+++ Problem_11.java
@@ -0,0 +1,29 @@
+// Problem 11: Container With Most Water
+
+class Problem_11 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Problem_11 solution = new Problem_11();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = solution.maxArea(height);
+        System.out.println("The maximum area of water that can be contained is: " + result);
+    }
+}
