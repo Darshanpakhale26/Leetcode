e05064649df0e5ca9a652a4c25f15d15e4732311
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-03-13 00:28:45 +0530

    first commit

---
+++ Problem_701.java
@@ -0,0 +1,16 @@
+// Problem 701: Insert into a Binary Search Tree
+
+class Problem_701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+    
+}
