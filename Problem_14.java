41028bd388b1bbbcfddd8aa86839f5bdd3a0f81c
Darshanpakhale26 <darshanvpakhale26@gmail.com>
2026-03-18 23:23:39 +0530

    first commit

---
+++ Problem_14.java
@@ -0,0 +1,30 @@
+// Problem 14: Longest Common Prefix
+
+class Problem_14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        Problem_14 solution = new Problem_14();
+        String[] strs = {"flower", "flow", "flight"};
+        String result = solution.longestCommonPrefix(strs);
+        System.out.println("The longest common prefix is: " + result);
+    }
+}
